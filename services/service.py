3ca3ba0d2d0a191ec138c4a480f3ae529fbd5a96
Andrew <andrew.freelance@i.ua>
2015-04-15 14:38:49 +0300

    some stage

--- services/service.py
+++ services/service.py
@@ -1,11 +1,31 @@
 from types import ModuleType, MethodType, ClassType
 
 import explorer
+import common
+from common import APIObject
 
 class Service(object):
-    def __init__(self,service_initial_object):
-        if isinstance (service_initial_object,'str'):
+    pass
+
+
+class ServiceBuilder(object):
+
+    def build(self,service_initial_object):
+
+        # ACCESSING MODULE BY PATH
+        if isinstance (service_initial_object,str):
             pass
+
+        # ACCESSING MODULE BY IMPORTED OBJECT
         if isinstance (service_initial_object, ModuleType):
             pass
+
+        # ACCESSING MODULE BY APIObject class instance
+        if isinstance (service_initial_object, APIObject):
+            pass
+
+        # ACCESSING MODULE BY EXPLORING host:port pare
+        if isinstance (service_initial_object, tuple):
+            pass
+
 
